7a15570e87b9c4a4d398afcb3b2ead5e4de2d11a
ram220 <vallururamakrishna9@gmail.com>
2026-03-17 22:46:19 +0530

    some changes in email sending

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: "smtp.gmail.com",
+    port:578,
+    secure:false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
